54ff83ea9c46b0f0f018222f91ff65b74e5b0602
aubrcart224 <aubrcart224@ed.amdsb.ca>
2024-05-02 21:12:22 -0400

    ,

--- Hello World Assem/HelloWorld.asm
+++ Hello World Assem/HelloWorld.asm
@@ -15,19 +15,19 @@ includelib kernel32.lib
 .code
 main proc
 	
-	sub rsp, 28h		;allocated shadow space for function calls 
+	sub rsp, 28h			;allocated shadow space for function calls 
 
-	move rcx, 0			;handle to parent window (NULL)
-	lea rdx, [CAP]		; Second param :dialog title 
+	move rcx, 0				;handle to parent window (NULL)
+	lea rdx, [CAP]			; Second param :dialog title 
 	lea r8, [Hello world]	; 3rd param : dialog message
-	mov r9d, 0			; 4th param : MB_OK
+	mov r9d, 0				; 4th param : MB_OK
 
-	call MessageBoxA	; call the MessageBoxA api
+	call MessageBoxA		; call the MessageBoxA api
 
-	add rsp, 28h		;clean up shadow space
+	add rsp, 28h			;clean up shadow space
 
-	mov ecx, 0			; exit code 
-	call ExitProcess	; Terminate the application 
+	mov ecx, 0				; exit code 
+	call ExitProcess		; Terminate the application 
 
 main endp
 
