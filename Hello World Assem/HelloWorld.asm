4585afb21307f8e89697e53e25521f8f3baa5b34
aubrcart224 <aubrcart224@ed.amdsb.ca>
2024-05-02 21:05:37 -0400

    should be working Hello world

--- Hello World Assem/HelloWorld.asm
+++ Hello World Assem/HelloWorld.asm
@@ -1,1 +1,34 @@
-section .data 
+.386
+.model flat, stdcall 
+option casemap:none
+
+incude windows.inc
+include user32.inc
+include kernel32.inc
+includelib user32.lib
+includelib kernel32.lib
+
+.data 
+	message db "Hello, World!", 0
+	caption db "HelloWorld", 0
+
+.code
+main proc
+	
+	sub rsp, 28h		;allocated shadow space for function calls 
+
+	move rcx, 0			;handle to parent window (NULL)
+	lea rdx, [caption]	; Second param :dialog title 
+	lea r8, [message]	; 3rd param : dialog message
+	mov r9d, 0			; 4th param : MB_OK
+
+	call MessageBoxA	; call the MessageBoxA api
+
+	add rsp, 28h		;clean up shadow space
+
+	mov ecx, 0			; exit code 
+	call ExitProcess	; Terminate the application 
+
+main endp
+
+end
